13d163c21defd3ad4a30cae942628f070367372e
Sijie Zhang <jsz@Sijies-MacBook-Air.local>
2022-10-09 21:07:16 -0400

    download swiper package and add swiper feature

--- Christine/reactweb/src/components/HeroSection.js
+++ Christine/reactweb/src/components/HeroSection.js
@@ -2,7 +2,50 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/modules/navigation/navigation.min.css';
+import 'swiper/modules/pagination/pagination.min.css';
+import 'swiper/modules/scrollbar/scrollbar.min.css';
+import 'swiper/modules/autoplay/autoplay.min.css';
 
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function imgUrl() {
+  const id = rand(1, 200);
+  return `https://picsum.photos/id/${id}/1920/1080`;
+}
+
+function createSlide() {
+  return (
+    <SwiperSlide>
+      <img className="img" src={imgUrl()} alt="" />
+    </SwiperSlide>
+  );
+}
+
+export default () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={1}
+      navigation
+      autoplay={true}
+      pagination={{ clickable: true }}
+    >
+      {createSlide()}
+      {createSlide()}
+      {createSlide()}
+      {createSlide()}
+    </Swiper>
+  );
+};
+
+/*
 function HeroSection() {
   return (
     <div className='hero-container'>
@@ -21,3 +64,5 @@ function HeroSection() {
 }
 
 export default HeroSection;
+*/
+
