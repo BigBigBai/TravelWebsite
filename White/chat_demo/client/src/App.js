75c38d8f87ac50762e21c8ef24abaaaa29ce35ce
Yingjian Bai <baiyingjian@outlook.com>
2022-10-12 12:35:21 -0400

    client side: redo due to npm start fail

--- White/chat_demo/client/src/App.js
+++ White/chat_demo/client/src/App.js
@@ -6,7 +6,10 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   return (
     <div className="App">
-      
+      <h3>Join A Chat</h3>
+      <input type="text" placeholder="White..." />
+      <input type="text" placeholder="Room ID..." />
+      <button>Join A Room</button> 
     </div>
   );
 }
