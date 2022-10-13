81b57c04f4dc1a2fdb94da5defc69861fce85dad
Yingjian Bai <baiyingjian@outlook.com>
2022-10-13 17:23:28 -0400

    client side: add messageList state to display what being sent

--- White/chat_demo/client/src/Chat.js
+++ White/chat_demo/client/src/Chat.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 function Chat({ socket, username, room }) {
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
 
     const sendMessage = async () => {
         if (currentMessage !== "") {
@@ -16,12 +17,14 @@ function Chat({ socket, username, room }) {
             };
 
             await socket.emit("send_message", messageData);
+            //be able to see what we send
+            setMessageList((list) => [...list, messageData]);
         }
     };
 
     useEffect(() => {
         socket.on("receive_message", (data) => {
-            console.log(data);
+            setMessageList((list) => [...list, data]);    
         })
     }, [socket]);    
 
@@ -30,14 +33,33 @@ function Chat({ socket, username, room }) {
             <div className='chat-header'>
                 <p>Live Chat</p>
             </div>
-            <div className='chat-body'></div>
+            <div className='chat-body'>
+                {messageList.map((messageContnet) => {
+                    return (
+                        <div className='message' id={username === messageContnet.author ? "you" : "other"}>
+                            <div>
+                                <div className='message-content'>
+                                    <p>{messageContnet.message}</p>
+                                </div>
+                                <div className='message-meta'>
+                                    <p id="time">{messageContnet.time}</p>
+                                    <p id="author">{messageContnet.author}</p>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>                
             <div className='chat-footer'>
                 <input 
                     type="text" 
                     placeholder="Hey..."
                     onChange={(event) => { 
                         setCurrentMessage(event.target.value); 
-                    }}  
+                    }} 
+                    onKeyPress={(event) => {
+                        event.key === "Enter" && sendMessage();
+                    }} 
                 />
                 <button onClick={sendMessage}>&#9658;</button>
             </div>
