d24442c6b3eb76de68ffd47a3227b349a2273ea2
Yingjian Bai <baiyingjian@outlook.com>
2022-10-24 23:34:45 -0400

    src/components

--- White/firebase_demo/src/App.js
+++ White/firebase_demo/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from './components/Navbar';
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
-
+        {/* Navbar */}
+        <Navbar />
       </section>
     </div>
   );
