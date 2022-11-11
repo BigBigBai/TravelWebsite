554997a9a2ae9fffb68176617b39faf1ac650466
Sijie Zhang <jsz@Sijies-Air.ht.home>
2022-11-11 14:53:16 -0500

    01_website add react-fullpage function

--- 01_website/src/components/pages/Home.js
+++ 01_website/src/components/pages/Home.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../HeroSection';
-import Cards from '../Cards';
+import './Home.css'
 import ReactFullpage from '@fullpage/react-fullpage';
 import Footer from '../Footer';
+import { Button } from '../Button';
+import HeroSection from '../HeroSection';
 
-function Home() {
+const Home = () => {
   return (
-    <>
-      <HeroSection />
-      <Cards />
-      <Footer />
-    </>
+    <ReactFullpage
+      scrollingSpeed={1000}
+      scrollHorizontally={true}
+      navigation={true}
+      sectionsColor={["orange", "purple", "green"]}
+      render={() => {
+        return (
+          <>
+            <div id="fullpage-wrapper">
+              <div className="section">
+                <HeroSection />
+                
+              </div>
+
+              <div className="section section1">
+                <h3>Section 2</h3>
+              </div>
+
+              <div className="section section2">
+                <h3>Section 3</h3>
+
+              </div>
+            </div>
+          </>
+        );
+      }}
+    />
   );
-}
+};
+
 
 export default Home;
